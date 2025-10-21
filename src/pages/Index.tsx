5a495c5ac44678bcf477a96e803cfc30ea4a65eb
[dyad] <git@dyad.sh>
2025-10-21 19:33:08 +0200

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -296,11 +296,9 @@ const Index = () => {
   const showMatkaAsMainTitle = !selectedJourney && !loadingJourneys && journeys.length === 0;
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50 p-4 sm:p-6 lg:p-8">
-      <div className="max-w-3xl mx-auto">
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50 p-4 sm:p-6 lg:p-8">
+      <div className="max-w-3xl mx-auto flex-grow w-full">
         <AppHeader />
-
-        {/* Removed the backend badge */}
 
         {/* NEW WELCOME SECTION FOR ADMIN REGISTRATION */}
         {!isAuthenticated && usersExist === false && (
