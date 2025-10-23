c8db40b93d3b0c5c9890a8303dcd4fa762a4d171
[dyad] <git@dyad.sh>
2025-10-23 21:17:24 +0200

    [dyad] Fixed Index.tsx syntax error by using shorthand React fragment - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -447,7 +447,7 @@ const Index = () => {
   const hasPostsWithCoordinates = posts.some(post => post.coordinates);
 
   return (
-    <React.Fragment>
+    <>
       <div className="flex-grow max-w-3xl mx-auto w-full p-4 sm:p-6 lg:p-8">
         {isAuthenticated ? (
           selectedJourney ? (
@@ -866,7 +866,7 @@ const Index = () => {
           }}
         />
       )}
-    </React.Fragment>
+    </>
   );
 };
 
