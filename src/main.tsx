fecfe1bfb4fb642e61ef70d2a9d23e300500eb51
[dyad] <git@dyad.sh>
2025-10-26 12:44:03 +0100

    [dyad] Updated the HTML title to be dynamically translated across supported languages and added a compass logo as the favicon. - wrote 7 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,47 @@ import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
-import { JourneyProvider } from "./contexts/JourneyContext.tsx"; // Import JourneyProvider
-import './i18n'; // Import i18n configuration
-import { I18nextProvider } from 'react-i18next'; // Import I18nextProvider
-import i18n from './i18n'; // Import the i18n instance
+import { JourneyProvider } from "./contexts/JourneyContext.tsx";
+import './i18n';
+import { I18nextProvider, useTranslation } from 'react-i18next'; // Import useTranslation
+import i18n from './i18n';
+import React, { useEffect } from "react"; // Import React and useEffect
+
+// Component to handle dynamic title updates
+const TitleUpdater = () => {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const updateTitle = () => {
+      document.title = t('app.htmlTitle');
+    };
+
+    // Set initial title
+    updateTitle();
+
+    // Update title when language changes
+    i18n.on('languageChanged', updateTitle);
+
+    // Cleanup event listener
+    return () => {
+      i18n.off('languageChanged', updateTitle);
+    };
+  }, [t]); // Re-run effect if 't' (translation function) changes
+
+  return null; // This component doesn't render anything
+};
 
 createRoot(document.getElementById("root")!).render(
-  <I18nextProvider i18n={i18n}> {/* Wrap with I18nextProvider */}
-    <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
-      <AuthProvider>
-        <JourneyProvider> {/* Wrap App with JourneyProvider */}
-          <App />
-        </JourneyProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  </I18nextProvider>
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <TitleUpdater /> {/* Add the TitleUpdater component here */}
+      <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
+        <AuthProvider>
+          <JourneyProvider>
+            <App />
+          </JourneyProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </I18nextProvider>
+  </React.StrictMode>
 );
